805137e157f763fd2e93707ccb7495904800d273
Eliezer Guevara <magdiel.852@gmail.com>
2023-07-03 10:29:34 -0600

    Codigo

--- game.js
+++ game.js
@@ -1,7 +1,17 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
+const btnUp = document.querySelector('#up')
+const btnDown = document.querySelector('#down')
+const btnRight = document.querySelector('#right')
+const btnLeft = document.querySelector('#left')
+
 let canvasSize; 
 let elementsSize ;
+
+const playerPosition = {
+    x: undefined,
+    y: undefined
+};
 
 window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize);
@@ -10,7 +20,6 @@ window.addEventListener('resize', setCanvasSize);
 
 
 function setCanvasSize() {
-    
     if (window.innerHeight > window.innerWidth) {
         canvasSize = window.innerWidth *0.75;
     } else {
@@ -32,9 +41,68 @@ function startGame() {
      game.font = elementsSize+'px Verdana';
      game.textAlign ='end'
      
-     for (let i = 1; i <= 10; i++) {
-         game.fillText(emojis['X'],elementsSize *i,elementsSize)
-         
-     }
- 
+    const map =maps[0];
+    const mapRows = map.trim().split('\n');
+    const mapRowsCols = mapRows.map(row=> row.trim().split(''));
+    console.log({map, mapRows, mapRowsCols});
+
+    mapRowsCols.forEach((row, rowI) => {
+        row.forEach( (col, colI) => {
+            const emoji = emojis[col];
+            const posX = elementsSize * (colI+1);
+            const posY = elementsSize * (rowI +1);
+
+            if (col == 'O') {
+                playerPosition.x = posX;
+                playerPosition.y = posY;
+                console.log({playerPosition});
+            }
+            game.fillText(emoji,posX,posY);
+
+            console.log({row, rowI,col,colI});
+        });
+    });
+
+    //  for (let row = 1; row <= 10; row++) {
+    //     for (let col = 1; col <= 10; col++) {
+    //          game.fillText(emojis[mapRowsCols[row-1][col-1]],elementsSize*col,elementsSize*row)
+            
+    //     }
+    // }
+    
+ }
+
+ btnUp.addEventListener('click',moveUp);
+ btnLeft.addEventListener('click',moveLeft);
+ btnDown.addEventListener('click',moveDown);
+ btnRight.addEventListener('click',moveRight);
+
+ window.addEventListener('keydown', moveByKeys);
+
+ function moveByKeys(event) {
+    if (event.key == 'ArrowUp') {
+        moveUp();
+    }else if (event.key == 'ArrowDown'){
+        moveDown();
+    }else if (event.key == 'ArrowRight'){
+        moveRight();
+    }else if (event.key == 'ArrowLeft'){
+        moveLeft();
+    }
+ }
+
+ function moveUp() {
+    console.log('Arriba');
+ }
+
+ function moveDown() {
+    console.log('Abajo');
+ }
+
+ function moveRight() {
+    console.log('Derecha');
+ }
+
+ function moveLeft() {
+    console.log('Izquierda');
  }
